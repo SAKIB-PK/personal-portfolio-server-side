d79033bc925c7b527ac59ed15465e06ddfd95804
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-12-05 07:04:22 +0600

    cors module add for cors policy block

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors')
 require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(cors())
 // MongoDB Connection 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rtuf5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
